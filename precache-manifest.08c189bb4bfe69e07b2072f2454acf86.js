88e8f1994b17b83854e53307f94132478b922592
j.bennett <61255945+jben817@users.noreply.github.com>
2020-07-18 23:29:29 -0400

    Updates

---
+++ precache-manifest.08c189bb4bfe69e07b2072f2454acf86.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6eaf5eaca45ea77f1727e6c78a2e1069",
+    "url": "/homework19app/index.html"
+  },
+  {
+    "revision": "97ae9c8454c8b476f1a3",
+    "url": "/homework19app/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "d5aec66801be8756131b",
+    "url": "/homework19app/static/js/2.bf4ca1fe.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/homework19app/static/js/2.bf4ca1fe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "97ae9c8454c8b476f1a3",
+    "url": "/homework19app/static/js/main.9e6e915a.chunk.js"
+  },
+  {
+    "revision": "1fae77af30e5efd52ede",
+    "url": "/homework19app/static/js/runtime-main.bb46b789.js"
+  }
+]);
